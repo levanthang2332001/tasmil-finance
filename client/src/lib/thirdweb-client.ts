8e014b956a4db53f54e160f02157d1d72626198f
nathanielfsn <phuongtn@mirailabs.co>
2025-04-21 22:53:32 +0700

    delete file client

--- client/src/lib/thirdweb-client.ts
+++
@@ -1,7 +0,0 @@
-import { createThirdwebClient, ThirdwebClient } from "thirdweb";
-
-export const client: ThirdwebClient = createThirdwebClient({
-  secretKey: process.env.THIRDWEB_SECRET_KEY || "",
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "",
-});
-
