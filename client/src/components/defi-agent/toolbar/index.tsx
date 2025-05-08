7f0ee21527267cfbbf76b794ecbeec96edd7c713
nathanielfsn <phuongtn@mirailabs.co>
2025-05-08 08:17:41 +0700

    chore: enhance sidebar functionality with onClick handlers

--- client/src/components/defi-agent/toolbar/index.tsx
+++ client/src/components/defi-agent/toolbar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ConnectWallet from "@/components/ConnectWallet";
+// import ConnectWallet from "@/components/ConnectWallet";
 import { cn } from "@/lib/utils";
 import Action from "../action";
 import Tabs from "../tab";
@@ -20,7 +20,7 @@ const ToolBar = ({ className }: ToolbarProps) => {
   return (
     <div className={cn("w-[360px] h-full border-l bg-background", className)}>
       <div className="flex flex-col h-full">
-        <ConnectWallet label="Connect" className="p-4 pb-0" />
+        {/* <ConnectWallet label="Connect" className="p-4 pb-0" /> */}
         <Action />
         <Tabs />
       </div>
