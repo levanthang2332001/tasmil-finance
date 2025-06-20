c019300d7ecb5ac16b27dae17360077ad28c0aff
Văn Thắng <vanthang23032001@gmail.com>
2025-06-20 16:22:30 +0700

    feat: add encryption and decryption utilities along with input validation for wallet generation

---
+++ server/src/utils/encrypt.ts
@@ -0,0 +1,22 @@
+import * as forge from "node-forge";
+import { IEncryptRequest, IEncryptResponse } from "../interfaces/crypto.interface";
+
+export const Encrypt = (params: IEncryptRequest): IEncryptResponse => {
+  const { prKey, password } = params;
+
+  const salt = forge.random.getBytesSync(16);
+  const iv = forge.random.getBytesSync(16);
+
+  const key = forge.pkcs5.pbkdf2(password, salt, 10000, 32);
+
+  const cipher = forge.cipher.createCipher('AES-CBC', key);
+  cipher.start({ iv: iv });
+  cipher.update(forge.util.createBuffer(prKey, "utf8"));
+  cipher.finish();
+
+  return {
+    cipherText: forge.util.encode64(cipher.output.getBytes()),
+    salt: forge.util.encode64(salt),
+    iv: forge.util.encode64(iv)
+  }
+}
