aa3004f684c761686602897c8f4e59c00561c907
Văn Thắng <vanthang23032001@gmail.com>
2025-07-04 13:43:56 +0700

    fix: refactor signature handling and nonce verification in ConnectButton and AuthController

--- server/src/wallet/guard/auth.module.ts
+++ server/src/wallet/guard/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './service/auth.service';
+import { RedisModule } from 'src/redis/redis.module';
+import { RedisCacheService } from 'src/redis/services/redisCacheService';
 
 @Module({
+  imports: [RedisModule],
   controllers: [AuthController],
+  providers: [AuthService, RedisCacheService],
 })
 export class AuthModule {}
