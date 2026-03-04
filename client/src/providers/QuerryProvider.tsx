5a4478fe9d6899611973f64356e420beec89f47d
le-dat <lqdat87@gmail.com>
2026-03-04 22:27:00 +0700

    Remove the default retry option from the React Query client configuration.

--- client/src/providers/QuerryProvider.tsx
+++ client/src/providers/QuerryProvider.tsx
@@ -3,13 +3,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 3,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 export function ReactQueryClientProvider({ children }: PropsWithChildren) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
