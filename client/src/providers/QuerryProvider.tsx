afb20d15ecc44666836c46206b91f7eaf56ccf7c
le-dat <lqdat87@gmail.com>
2026-03-04 22:27:43 +0700

    refactor: move QueryClient instantiation into ReactQueryClientProvider component

--- client/src/providers/QuerryProvider.tsx
+++ client/src/providers/QuerryProvider.tsx
@@ -3,8 +3,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
-const queryClient = new QueryClient();
-
 export function ReactQueryClientProvider({ children }: PropsWithChildren) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
